44ccc49cb39946000cf6bd99a7fe643605b6a236
jahidunfarabi <jahidunmuntaka25@gmail.com>
2026-08-19 01:24:01 +0600

    Add project files.

---
+++ Task3LCMApp/Controllers/LcmController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Task3LCMApp.Controllers
+{
+    [ApiController]
+    [Route("jahidunmuntaka25_gmail_com")] //my email
+    public class LcmController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetLcm([FromQuery] string? x, [FromQuery] string? y)
+        {
+            if (!long.TryParse(x, out long numberX) || !long.TryParse(y, out long numberY))
+            {
+                return Content("NaN", "text/plain");
+            }
+
+            if (numberX <= 0 || numberY <= 0)
+            {
+                return Content("NaN", "text/plain");
+            }
+
+            try
+            {
+                long lcm = CalculateLCM(numberX, numberY);
+                return Content(lcm.ToString(), "text/plain");
+            }
+            catch
+            {
+                return Content("NaN", "text/plain");
+            }
+        }
+
+        private long CalculateGCD(long first, long second)
+        {
+            while (second != 0)
+            {
+                long remainder = first % second;
+                first = second;
+                second = remainder;
+            }
+            return first;
+        }
+
+        private long CalculateLCM(long numberX, long numberY)
+        {
+            long gcd = CalculateGCD(numberX, numberY);
+            return (numberX / gcd) * numberY;
+        }
+    }
+}
